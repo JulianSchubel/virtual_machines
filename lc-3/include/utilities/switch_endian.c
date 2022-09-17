180d71cf4e43213eca722444d70072e7cf101f81
Julian Schubel <schubeljulian@gmail.com>
2022-09-17 16:00:59 +0200

    Endianness comment

--- lc-3/include/utilities/switch_endian.c
+++ lc-3/include/utilities/switch_endian.c
@@ -3,6 +3,11 @@
 
 #include <stdint.h>
 
+/* 
+    Endianness refers to how bytes are interpreted.
+    Big-endian: Least significant byte at the largest memory address
+    Little-endian: Least significant byte at the smallest memory address
+*/
 uint16_t switch_endian(uint16_t x)
 {
 	/* General algorithm for unsigned integers: x << (bit_size/2) | x >> (bit_size/2) */
