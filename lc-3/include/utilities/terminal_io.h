458e651383a1ddb0d7333a78fc56ffba5e293e25
Julian Schubel <schubeljulian@gmail.com>
2022-09-19 07:58:48 +0200

    Resolved conflicts readded Makefile

--- lc-3/include/utilities/terminal_io.h
+++ lc-3/include/utilities/terminal_io.h
@@ -3,7 +3,6 @@
 
 #include <sys/termios.h>
 
-<<<<<<< HEAD
 /* Disable canonical (lin-by-line) input and echoing of input */
 void disable_input_buffering();
 /* Restore original terminal configuration */
@@ -12,14 +11,11 @@ void restore_input_buffering();
 void handle_interrupt(int signal);
 
 /* Structure to save original terminal configuration */
-=======
 //Set up terminal input
->>>>>>> 19eeeb56f9cb17c6bfefc35c1b9ee316937041c0
 struct termios original_tio;
 
 void disable_input_buffering()
 {
-<<<<<<< HEAD
     /* Save current terminal configuration */
     tcgetattr(STDIN_FILENO, &original_tio);
     struct termios new_tio = original_tio;
@@ -29,13 +25,10 @@ void disable_input_buffering()
      */
     new_tio.c_lflag &= ~ICANON & ~ECHO;
     /* TCSANOW: apply immediately */
-=======
     tcgetattr(STDIN_FILENO, &original_tio);
-    struct termios new_tio = original_tio;
     /* Turn off canonical (line-by-line) input, and echoing of input characters */
     new_tio.c_lflag &= ~ICANON & ~ECHO;
     /* Apply terminal configuration immediately (TCSANOW) */
->>>>>>> 19eeeb56f9cb17c6bfefc35c1b9ee316937041c0
     tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 }
 
@@ -45,7 +38,7 @@ void restore_input_buffering()
     tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
 }
 
-//Interrupt handling: Call restore_input_buffering on interrupt
+/* Interrupt handling: Call restore_input_buffering on interrupt */
 void handle_interrupt(int signal)
 {
     restore_input_buffering();
